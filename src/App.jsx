101ba78dca059475de05c1832fdbc7d6c75b003f
torres_m <torres_m@ca01.ub.lan>
2026-03-09 11:50:39 +0800

    Created navigation component and setup the router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-function App(){
-  return(
+import { Routes, Route } from "react-router-dom"
+import Navigation from "./components/Navigation"
+import Home from "./pages/Home"
+import Students from "./pages/Students"
+import StudentDetails from "./pages/StudentDetails"
+import About from "./pages/About"
+
+function App() {
+  return (
     <div>
-      
+
+      <Navigation />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/students" element={<Students />} />
+        <Route path="/student/:id" element={<StudentDetails />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
     </div>
   )
 }
+
+export default App
