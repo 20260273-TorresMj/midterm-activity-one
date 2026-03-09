39e29606f1c91a6c4ba0118ea828067da62f59fa
torres_m <torres_m@ca01.ub.lan>
2026-03-09 11:44:40 +0800

    Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+ function Home() {
+    return(
+        <div>
+            <h2>Welcome to Student Dashboard</h2>
+            <p>This app demonstrates React Router, useState, and useEffect</p>
+        </div>
+    )
+
+}
+
+export default Home
